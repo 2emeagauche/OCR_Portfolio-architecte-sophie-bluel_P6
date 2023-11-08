edf76e399e669177096f2df4fc9776dd38f12292
2emeagauche <pychassaigne@gmail.com>
2023-11-08 20:49:59 +0100

    designing the modal content step 1

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -41,6 +41,28 @@ if(adminMode) {
   loginCta.innerText = "Logout";
   // Apply reset routine to logout
   loginCta.addEventListener("click", (e) => disableAdminMode(e), {once: true});
+
+  const adminModal = document.getElementById("admin-modal");
+  const protectFromCloseEvent = document.querySelector(".protect-from-close-event");
+  const buttonEdit = document.querySelector(".edit-gallery");
+  const buttonCloseModal = document.querySelector(".close-modal");
+
+  adminModal.showModal();
+  buttonEdit.addEventListener("click", () => {
+    adminModal.showModal();
+  });
+
+  buttonCloseModal.addEventListener("click", () => {
+    adminModal.close();
+  });
+
+  adminModal.addEventListener("click", () => {
+    adminModal.close();
+  });
+
+  protectFromCloseEvent.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
 }
 
 // On logout we remove admin mode specific styles and session infos
