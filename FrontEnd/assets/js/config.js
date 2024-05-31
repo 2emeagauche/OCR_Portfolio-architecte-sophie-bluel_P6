7cc4707a3a66b266d378c50fc2ff80b5f763bc03
2emeagauche <pychassaigne@gmail.com>
2024-05-31 16:44:22 +0200

    api on onrender.com

--- FrontEnd/assets/js/config.js
+++ FrontEnd/assets/js/config.js
@@ -1,4 +1,5 @@
-const apiLocalPath = "http://localhost:5678/api";
+// const apiLocalPath = "http://localhost:5678/api";
+const apiLocalPath = "https://ocr-portfolio-architecte-sophie-bluel-p6.onrender.com/api";
 
 const portfolioElement = document.querySelector("#portfolio");
 const galleryElement = document.querySelector(".gallery");
