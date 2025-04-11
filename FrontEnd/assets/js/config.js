90d1b09ef59d9e487fa927be5e1b59f45ac5e5d6
2emeagauche <pychassaigne@gmail.com>
2025-04-11 15:52:37 +0200

    online backend on Railway.app + delete dummy "contact" menu link

--- FrontEnd/assets/js/config.js
+++ FrontEnd/assets/js/config.js
@@ -1,5 +1,5 @@
 // const apiLocalPath = "http://localhost:5678/api";
-const apiLocalPath = "https://ocr-portfolio-architecte-sophie-bluel-p6.onrender.com/api";
+const apiLocalPath = "https://web-production-ded9.up.railway.app";
 
 const portfolioElement = document.querySelector("#portfolio");
 const galleryElement = document.querySelector(".gallery");
